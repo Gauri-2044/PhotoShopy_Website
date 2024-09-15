3c2663d9499aac90fccd8c90213325118a06d685
Amisha-2004 <amishabhasme2004@gmail.com>
2024-09-14 23:19:34 -0700

    FirstCommit

---
+++ script.js
@@ -0,0 +1,10 @@
+const url1 = "https://jsonplaceholder.typicode.com/photos";
+
+// console.log("Hello");
+
+// const promise = fetch(url).then((res1)=>{
+//     data1 = res1.json();
+// });
+//     console.log(data1);
+
+
